008ab502bedfc923e6248d53a171fcdb436ba47f
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2024-01-17 22:40:47 +0700

    finish services

---
+++ src/components/Service/Service.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import img2 from "../../assets/rendang.png"
+
+const ServicesData = [
+    {
+        id: 1,
+        img: img2,
+        name: 'Rendang',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, similique?'
+    },
+    {
+        id: 2,
+        img: img2,
+        name: 'Sate Padang',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, similique?'
+    },
+    {
+        id: 3,
+        img: img2,
+        name: 'Gulai Ayam',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, similique?'
+    },
+]
+
+const Service = () => {
+    return (
+        <>
+            <div className='py-10'>
+                <div className="container">
+                    <div className='text-center mb-20 max-w-[400px] mx-auto'>
+                        <p className='text-sm bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary'>Our Services</p>
+                        <h1 className='text-3xl font-bold'>Services</h1>
+                        <p className='text-xs text-gray-400'>
+                            {" "}
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil repellendus in sed! Provident a commodi quisquam esse blanditiis culpa earum!
+                        </p>
+                    </div>
+
+                    {/* card section */}
+                    <div>
+                        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-14 md:gap-5 place-items-center' >
+                            {
+                                ServicesData.map(({ id, img, name, description }) => {
+                                    return <div key={id} className='rounded-2xl bg-white dark:bg-gray-800 hover:bg-primary dark:hover:bg-primary hover:text-white relative shadow-xl duration-high group max-w-[300px]'>
+                                        <div className='h-[130px]'>
+                                            <img src={img} alt="" className='max-w-[200px] block mx-auto transform -translate-y-14
+                  group-hover:scale-105 group-hover:rotate-6 duration-300' />
+                                        </div>
+                                        <div className='p-4 text-center'>
+                                            <h1 className='text-xl font-bold dark:text-white'>{name}</h1>
+                                            <p className='text-gray-500 text-sm line-clamp-2 group-hover:text-white duration-300'>{description}</p>
+                                        </div>
+                                    </div>
+                                })
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Service
