c713e845cbafbf66d67430b624509461545f8102
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2024-01-18 15:20:13 +0700

    add testimoni

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,20 @@ import Hero from './components/Hero/Hero'
 import Service from './components/Service/Service'
 import Banner from './components/Banner/Banner'
 import AppStore from './components/AppStore/AppStore'
+import Testimonial from './components/Testimonial/Testimonial'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
       <Navbar/>
       <Hero/>
       <Service/>
       <Banner/>
       <AppStore/>
-    </>
+      <Testimonial/>
+    </div>
   )
 }
 
